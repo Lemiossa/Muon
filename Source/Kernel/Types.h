1d26f14162500f2072219f4c2c5c19280c469ed1
Matheus Leme Da Silva <matheuslemedasilva000@gmail.com>
2026-01-28 09:38:45 -0300

    Kernel: Easying the life

--- Source/Kernel/Types.h
+++ Source/Kernel/Types.h
@@ -14,6 +14,31 @@ typedef int I32;
 typedef unsigned long long U64;
 typedef long long I64;
 
+typedef unsigned long int UPTR;
+
 #define NULL ((void *)0)
+
+extern U8 __text_start_addr;
+#define __TEXT_START_ADDR ((void *)&__text_start_addr)
+extern U8 __text_end_addr;
+#define __TEXT_END_ADDR ((void *)&__text_end_addr)
+
+extern U8 __data_start_addr;
+#define __DATA_START_ADDR ((void *)&__data_start_addr)
+extern U8 __data_end_addr;
+#define __DATA_END_ADDR ((void *)&__data_end_addr)
+
+extern U8 __bss_start_addr;
+#define __BSS_START_ADDR ((void *)&__bss_start_addr)
+extern U8 __bss_end_addr;
+#define __BSS_END_ADDR ((void *)&__bss_end_addr)
+
+extern U8 __start_addr;
+#define __START_ADDR ((void *)&__start_addr)
+extern U8 __end_addr;
+#define __END_ADDR ((void *)&__end_addr)
+
+#define ALIGN_UP(v, a) (((v) + ((a) - 1)) & ~((a) - 1))
+#define ALIGN_DOWN(v, a) ((v) & ~((a) - 1))
 
 #endif // TYPES_H
